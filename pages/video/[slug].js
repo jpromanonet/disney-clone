58ca90756ba66568f03d31d9aebca3edafb99244
jpromanonet <programming@jpromano.net>
2023-01-19 22:36:53 -0300

    slug search

--- pages/video/[slug].js
+++ pages/video/[slug].js
@@ -3,6 +3,7 @@ import {gql, GraphQLClient} from 'graphql-request'
 export const getServerSideProps = async (pageContext) => {
     const pageSlug = pageContext.query.slug
 
+    console.log(pageSlug)
 }
 const Video = () => {
     return (
