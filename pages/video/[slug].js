94c48f70b771f130a7acab2ab04f3a355ec08c6d
jpromanonet <programming@jpromano.net>
2023-01-20 13:59:43 -0300

    Video connection

--- pages/video/[slug].js
+++ pages/video/[slug].js
@@ -1,9 +1,13 @@
 import {gql, GraphQLClient} from 'graphql-request'
 
 export const getServerSideProps = async (pageContext) => {
+    const url = process.env.ENDPOINT
+    const graphQLClient = new GraphQLClient(url,{
+        headers: {
+            "Authorization": process.env.GRAPH_CMS_TOKEN
+        }
+    })
     const pageSlug = pageContext.query.slug
-
-    console.log(pageSlug)
 }
 const Video = () => {
     return (
